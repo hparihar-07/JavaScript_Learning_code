c24fa96b5764d54f2a41e3a8d0a9f6fbdf3af530
Parihar07 <73985710+hparihar-07@users.noreply.github.com>
2023-07-31 09:04:55 +0530

    Update Variables&DataTypes.js

--- Variables&DataTypes.js
+++ Variables&DataTypes.js
@@ -1,46 +1,38 @@
-// SPDX-License-Identifier: MIT
-pragma solidity >= 0.7.0<0.9.0;
-contract DataTypes{
-    //There are mainly four datatypes
-    //1 uint -> unsigned integer(only positive values)
-    // uint_types     storing_capacity    range:
-    // uint256        32bytes             0-(2^n - 1)
-    // uint128        16bytes
-    // uint64         8bytes
-    // uint32         4bytes
-    // uint16         2bytes
-    // uint8          1bytes
-    //Declaring a variable
-    uint myVar1 = 12;//By default uint will be uint256
+/**  • Variables in Js
+ •Variables can be declared by using var, let or const keywords
+*/
+//var can't be redeclared and updated within its scope
+var myVar1 = 7;
+console.log(myVar1);
+const ign ="Lord";//declares a constant value
+console.log(ign);
+let myVar2 = 07;
+console.log(myVar2);
+//let can be updated but bot redeclared
 
-    
-    //2 int -> integer(positive and negative  values)
-    // int_types     storing_capacity     range
-    // int256        32bytes              [{-2^(n-1)}-{2^(n-1) - 1}]
-    // int128        16bytes
-    // int64         8bytes
-    // int32         4bytes
-    // int16         2bytes
-    // int8          1bytes
-    //Declaring a variable
-    int myVar2 = -12;//By default uint will be int256
-    
-     
-     // 3 bool-> boolean(either true or false value)
-     // By default it is always false
-     bool myValue;
-
-
-     // 4 address (use to store ethereum address)
-     // Default value will be like 0x0000000000....
-     // it posses 160bits i.e 20bytes
-
-
-     // TypeCasting in Solidity
-     int y = 9;
-     uint x = 21;
-     function op() public{
-         x = x + uint(y);
-     }
-
+/** DataTypes in Js
+There are 7 primitive DataTypes in Js
+      🔰 NN SS BB U 🔰
+•Null      
+•Number    
+•String
+•Symbol
+•Boolean
+•Big Int
+•Undefined
+*/
+let a = null; 
+let b = 345; 
+let c = true; // can also be false  
+let d = BigInt("567") + BigInt("3") 
+let e = "Lord" 
+let f = Symbol("I am a nice symbol") 
+let g
+//Objects in Js
+const troop = {
+  "pekka" : true,
+  "dragon" : 7,
+  "Warden" : false,
+  "goblin" : null
 }
+console.log(troop["pekka"]);
