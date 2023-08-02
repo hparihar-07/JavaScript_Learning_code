a6046f106712482d0bb105f222314427ac62eb0f
Parihar07 <73985710+hparihar-07@users.noreply.github.com>
2023-08-02 08:33:24 +0530

    Add files via upload

---
+++ Js_Operators.js
@@ -0,0 +1,52 @@
+console.log("Operators in Js");
+//Arithmetic Operators
+console.log("1.Arithmetic Operators in Js");
+let a=7;
+let b=3;
+console.log("a + b =",a+b);
+console.log("a - b =", a-b);
+console.log("a * b =", a*b);
+console.log("a / b =", a/b);
+console.log("a % b =", a%b);
+console.log("a ** b =", a**b);
+console.log("a++ = ",a++);
+console.log("a-- = ", a--),
+console.log("a = ",a);
+console.log("++a = ", ++a),
+console.log("--a = ", --a)
+
+//Assignment Operators
+console.log("Assignment Operators in Js");
+let asg=7;
+asg += 5 // same as asg = asg + 5 
+ console.log("asg is now = ", asg) 
+ asg -= 5 // same as asg = asg - 5 
+ console.log("asg is now = ", asg) 
+ asg *= 5 // same as asg = asg * 5 
+ console.log("asg is now = ", asg) 
+ asg /= 5 // same as asg = asg / 5 
+ console.log("asg is now = ", asg)
+
+// Comparison Operators  
+console.log("Comparison Operators in Js")
+ let comp1 = 6; 
+ let comp2 = 7; 
+ console.log("comp1 == comp2 is ", comp1 == comp2) 
+ console.log("comp1 != comp2 is ", comp1 != comp2) 
+ console.log("comp1 === comp2 is ", comp1 === comp2) 
+ console.log("comp1 !== comp2 is ", comp1 !== comp2) 
+ console.log("comp1 > comp2 is ", comp1 > comp2) 
+  
+ // Logical Operators 
+ console.log("Logical Operators in Js")
+ let x = 5; 
+ let y = 6; 
+ console.log(x<y && x==5) 
+ console.log(x>y || x==5) 
+ console.log(!false) 
+ console.log(!true) 
+  
+ // Comments 
+ // This is a single line comment 
+ /* I am 
+ a  multiline comment*/
