86279315646cfdafa28e331b58e57b6174fb8ceb
Parihar07 <73985710+hparihar-07@users.noreply.github.com>
2023-08-07 07:59:38 +0530

    Add files via upload

---
+++ forOfLoop.js
@@ -0,0 +1,16 @@
+//For of loop in Js 
+
+let obj = { 
+   player: "lord", 
+   townhall: 8, 
+   exp: 254, 
+   ws: 3800, 
+   troop: "pekka"
+ } 
+  
+for (let b of "player") { 
+   console.log(b) 
+ }
+for (let b of "troop") { 
+   console.log(b) 
+ }
