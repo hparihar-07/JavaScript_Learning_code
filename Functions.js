f7d46c902b4dc3c5a5dcb0051936197e8f453c95
Parihar07 <73985710+hparihar-07@users.noreply.github.com>
2023-08-11 08:22:35 +0530

    Add files via upload

---
+++ Functions.js
@@ -0,0 +1,23 @@
+//Function to calculate sum
+ const sum = (a, b) => { 
+   return a + b 
+}
+ console.log(sum(9, 7));
+
+//Function to calculate difference
+const diff = (c, d) => {
+    return c - d;
+}
+console.log(diff(5, 2));
+
+//Function to multiply two nums
+const mul = (p, q) => {
+    return p * q;
+}
+console.log(mul(5, 2));
+
+//Function to divide two nums
+const dvd = (x, y) => {
+    return x / y;
+}
+console.log(dvd(10, 2));
