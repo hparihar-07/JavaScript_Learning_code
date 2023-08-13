cae10d3881e3d30a1d84f71b11f04a5ea3cad53b
Parihar07 <73985710+hparihar-07@users.noreply.github.com>
2023-08-13 06:23:47 +0530

    Add files via upload

---
+++ strings.js
@@ -0,0 +1,16 @@
+let word = "Hello World" 
+ // console.log(word.length) 
+ console.log(word[0]) 
+ console.log(word[6]) 
+ let friend = 'snow-e' 
+ console.log(friend) 
+  
+ // Template literals 
+ let t1 = "Pekka" 
+ let t2 = "Dragon" 
+ let sentence = `${t2} unlocks in a lower level barracks than  ${t1}` 
+ console.log(sentence) 
+  
+ // Escape Sequence Characters 
+ let warning = "Don\'t do this!" 
+ console.log(warning)
