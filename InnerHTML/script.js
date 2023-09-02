f227afaeb1837551302207b7a24fdfa18846b30d
Parihar07 <73985710+hparihar-07@users.noreply.github.com>
2023-09-02 07:43:50 +0530

    Create script.js

---
+++ InnerHTML/script.js
@@ -0,0 +1,11 @@
+let x = document.getElementsByTagName('span')[0] 
+ console.log(x) 
+ let y = document.getElementsByTagName('span')[0] 
+ console.dir(y) 
+ console.log(document.body.firstChild.nodeName)  
+console.log(document.body.firstElementChild.nodeName) 
+document.getElementById("first").innerHTML = "<i>hey I am italic</i>"  
+
+first.outerHTML = "<div>hey</div>" 
+document.body.firstChild.data  
+console.log(document.body.textContent)
