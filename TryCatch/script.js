164ab7f0035a6bce5cb690c21ef8ac48221e8876
Parihar07 <73985710+hparihar-07@users.noreply.github.com>
2023-09-17 07:29:22 +0530

    Create script.js

---
+++ TryCatch/script.js
@@ -0,0 +1,25 @@
+setTimeout(()=>{ 
+ console.log("Hacking wifi.... Please wait..." )  
+ }, 1000) 
+  
+ try{ 
+         setTimeout(()=>{  
+                 console.log("Target")   
+         }, 100) 
+ } 
+ catch(err){ 
+         console.log("Check your Command") 
+ } 
+  
+  
+ setTimeout(()=>{  
+         console.log("Fetching username and password.... Please wait..." ) 
+ }, 2000)  
+  
+ setTimeout(()=>{  
+ console.log("Hacking Target's account.... Please wait..." ) 
+ }, 3000)  
+  
+ setTimeout(()=>{  
+ console.log("Username and password of Target fetched.... Please wait..." ) 
+ }, 4000) 
