35814d6698b5e70d604b2dec8c291cda672a8ad6
Parihar07 <73985710+hparihar-07@users.noreply.github.com>
2023-09-19 08:09:50 +0530

    Create script.js

---
+++ Finally/script.js
@@ -0,0 +1,20 @@
+const f = () => { 
+         try { 
+                 let a = 0; 
+                 console.log("Program ran successfully") 
+                 return 
+         } 
+         catch (err) { 
+                 console.log("This is an error") 
+                 console.log(p) 
+         } 
+         finally { 
+                 console.log("I will run for sure don't worry") 
+                 // Close the file 
+                 // Exit the Loop 
+                 // Write to the log file 
+         } 
+ } 
+  
+ f() 
+ console.log("End")
