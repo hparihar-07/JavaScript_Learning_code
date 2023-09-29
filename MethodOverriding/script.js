644c5f09698455d4290ade57575e5e54eb6c8ff8
Parihar07 <73985710+hparihar-07@users.noreply.github.com>
2023-09-29 08:47:48 +0530

    Create script.js

---
+++ MethodOverriding/script.js
@@ -0,0 +1,38 @@
+class Employee { 
+   constructor(name) { 
+     console.log(`${name} - Employee's constructor is here`) 
+     this.name = name 
+   } 
+   login() { 
+     console.log(`Employee has logged in`); 
+   } 
+  
+   logout() { 
+     console.log(`Employee has logged out`); 
+   } 
+  
+   requestLeaves(leaves) { 
+     console.log(`Employee has requested ${leaves} leaves - Auto approved`) 
+   } 
+ } 
+  
+ class Programmer extends Employee { 
+   constructor(name) { 
+     super(name) 
+     console.log(`This is a newly written constructor`) 
+   } 
+
+   requestCoffee(x) { 
+     console.log(`Employee has requested ${x} coffees`) 
+   } 
+   requestLeaves(leaves) { 
+     super.requestLeaves(4) 
+     console.log("One extra is granted") 
+  
+   } 
+ } 
+  
+ let emp = new Programmer("Axel") 
+ emp.login() 
+ emp.requestLeaves(3)
+ emp.requestCoffee(2)
