e81d6846129e0bfafd09bb386f580c7ddb9c0c93
Parihar07 <73985710+hparihar-07@users.noreply.github.com>
2023-10-03 08:52:31 +0530

    Create script.js

---
+++ IIFE/script.js
@@ -0,0 +1,16 @@
+let a = () => { 
+   return new Promise((resolve, reject) => { 
+     setTimeout(() => { 
+       resolve(777) 
+     }, 3000) 
+   }) 
+ } 
+  
+ (async () => { 
+   let b = await a() 
+   console.log(b) 
+   let c = await a() 
+   console.log(c) 
+   let d = await a() 
+   console.log(d) 
+ })()
